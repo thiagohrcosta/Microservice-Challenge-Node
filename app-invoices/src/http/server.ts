f825182ba432f44f1334223a1632d755ec76898f
thiagohrcosta <thiagohrcosta86@gmail.com>
2025-06-06 23:36:39 -0300

    Remove useless code

--- app-invoices/src/http/server.ts
+++ app-invoices/src/http/server.ts
@@ -1,16 +1,13 @@
 import { fastify } from 'fastify'
-import { randomUUID } from 'node:crypto'
 import { fastifyCors } from '@fastify/cors'
 import { z } from 'zod'
+
 import {
   serializerCompiler,
   validatorCompiler,
   type ZodTypeProvider,
 } from 'fastify-type-provider-zod'
-import { channels } from '../broker/chanels/index.ts'
-import { db } from '../db/client.ts'
-import { schema } from '../db/schema/index.ts'
-import { dispatchOrderCreated } from '../broker/messages/order-created.ts'
+
 
 const app = fastify().withTypeProvider<ZodTypeProvider>()
 
